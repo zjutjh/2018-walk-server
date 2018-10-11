2fa605a941ac3dbc917adeb2d0188dbf48017a49
zzhutianyu <704735206@qq.com>
2018-10-11 21:28:18 +0800

    fix createGroup

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -28,7 +28,7 @@ class GroupController extends Controller
         $teamInfo = $request->all();
 
         $user = Auth::user();
-        if (!$user->yx_group_id) {
+        if (!!$user->yx_group_id) {
             return RJM(-1, '你已经拥有队伍');
         }
         $teamInfo['captain_id'] = $user->id;
