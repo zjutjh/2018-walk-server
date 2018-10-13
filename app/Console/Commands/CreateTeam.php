4bf05c795e32449fc9689bda9713e7c247bef503
zzhutianyu <704735206@qq.com>
2018-10-13 11:39:37 +0800

    add create success

---
+++ app/Console/Commands/CreateTeam.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\SuccessTeam;
+use App\YxGroup;
+use Illuminate\Console\Command;
+
+class CreateTeam extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:success';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $groups = YxGroup::whereNotNull('up_to_standard')->get();
+        foreach ($groups as $group) {
+            $success = new SuccessTeam();
+            echo "{$success->id}:{$group->id}:{$group->name}\r\n";
+            $success->yx_group_id = $group->id;
+            $success->save();
+        }
+    }
+}
