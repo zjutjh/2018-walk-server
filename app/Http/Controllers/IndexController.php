ef0b3d129be4f7f820972fba647283164a39a368
zzhutianyu <704735206@qq.com>
2018-10-20 09:43:36 +0800

    update count

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -39,11 +39,13 @@ class IndexController extends Controller
         $apply_count = User::getUserCount();
         $team_count = YxGroup::getTeamCount();
         $upToTeam = YxGroup::whereNotNull('up_to_standard')->where('select_route', '<>', '朝晖京杭大运河毅行')->count();
+        $chToTeam = YxGroup::whereNotNull('up_to_standard')->where('select_route', '朝晖京杭大运河毅行')->count();
+
 //        $res  = '报名人数: ' . $apply_count . '<br>';
 //        $res .= '队伍总数: ' . $team_count . '<br>';
 //        $res .= '达到要求队伍数: '. $upToTeam . '<br>';
 //        return $res;
-        return view('count', ['apply_count' => $apply_count, 'team_count' => $team_count, 'upToTeam' => $upToTeam]);
+        return view('count', ['apply_count' => $apply_count, 'team_count' => $team_count, 'upToTeam' => $upToTeam, 'ch' => $chToTeam]);
     }
 
 
--- resources/views/count.blade.php
+++ resources/views/count.blade.php
@@ -10,7 +10,8 @@
 <body>
 <p>报名人数: {{ $apply_count }}</p>
 <p>队伍总数: {{ $team_count }}</p>
-<p>达到要求队伍数: {{ $upToTeam }}</p>
+<p>屏峰达到要求队伍数: {{ $upToTeam }}</p>
+<p>朝晖达到要求队伍数: {{ $ch }}</p>
 <button ><a href="{{ route('user') }}">获取用户表格</a> </button>
 <button><a href="{{ route('group') }}">获取队伍表格</a> </button>
 
